f2e7090f553c7127a41b1ae401236a8e617e7458
Сергей Субботин <zarodiny@yandex.ru>
2023-03-20 20:27:57 +1000

    feat: добавляет компонент главная страница

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const CARDS_COUNT = 5;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App offersCount={CARDS_COUNT} />
   </React.StrictMode>,
 );
